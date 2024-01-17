481eac90b6fb9ae36c3701f386598152bea1cbbf
Alina Salakhova <alina.salakhova1991@gmail.com>
2024-01-17 22:43:31 +0300

    Homework_2: Добавление и обновление тестов для класса Item: тесты для сеттера name, instantiate_from_csv и string_to_number

--- tests/test_item.py
+++ tests/test_item.py
@@ -20,3 +20,30 @@ def test_apply_discount():
     Item.pay_rate = 0.8  # Скидка 20%
     item.apply_discount()
     assert item.price == 80.0
+
+
+def test_name_getter():
+    item = Item("Тестовый товар", 100.0, 5)
+    assert item.name == "Тестовый товар"
+
+
+def test_name_setter():
+    item = Item("Товар", 100.0, 5)
+    # Тестирование усечения длинного имени
+    item.name = "ОченьДлинноеНазвание"
+    assert item.name == "ОченьДлинн"  # Первые 10 символов
+
+    # Тестирование короткого имени
+    short_name = "Короткое"
+    item.name = short_name
+    assert item.name == short_name
+
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv('src/items.csv')
+    assert len(Item.all) > 0  # Проверяем, что список не пуст
+
+
+def test_string_to_number():
+    assert Item.string_to_number("123") == 123
+    assert Item.string_to_number("123.45") == 123
